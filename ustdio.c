3cfe9b65854f49cf29643e00f6620482819dc4e5
GoodMorning <yangxiangzhan@outlook.com>
2019-07-17 22:15:22 +0800

    添加 printk 格式输出支持

--- ustdio.c
+++ ustdio.c
@@ -114,302 +114,334 @@ int fputc(int ch, FILE *f)
 
 
 
-#if 1
-/**
-	* @author   古么宁
-	* @brief    i_itoa 
-	*           整型转十进制字符串
-	* @param    strbuf   转字符串所在内存
-	* @param    value  值
-	* @return   转换所得字符串长度
-*/	
-int i_itoa(char * strbuf,int value)		
+#define ZEROPAD 1		/* pad with zero */
+#define SIGN    2		/* unsigned/signed long */
+#define PLUS    4		/* show plus */
+#define SPACE   8		/* space if plus */
+#define LEFT    16		/* left justified */
+#define SMALL   32		/* Must be 32 == 0x20 */
+#define SPECIAL	64		/* 0x */
+
+static char *number(char *str, long num,int base, int size, int precision,
+                    int type)
 {
-	int len = 0;
-	int value_fix = (value<0)?(0-value) : value; 
-	
-	do
-	{
-		strbuf[len++] = (char)(value_fix % 10 + '0'); 
-		value_fix = value_fix/10;
+	/* we are called with base 8, 10 or 16, only, thus don't need "G..." */
+	static const char digits[16] = "0123456789ABCDEF"; /* "GHIJKLMNOPQRSTUVWXYZ"; */
+	unsigned long unum ;
+	char tmp[66];
+	char sign, locase,padding;
+	int chgsize;
+
+	if (base < 2 || base > 36)
+		return NULL;
+
+	/* locase = 0 or 0x20. ORing digits or letters with 'locase'
+	 * produces same digits or (maybe lowercased) letters */
+	locase = (type & SMALL);
+	if (type & LEFT)
+		type &= ~ZEROPAD;  
+
+	padding = (type & ZEROPAD) ? '0' : ' ';
+	sign = 0;
+	if (type & SIGN) {
+		if (num < 0) {
+			sign = '-';
+			num = -num;
+			size--;
+		} 
+		else 
+		if (type & PLUS) {
+			sign = '+';
+			size--;
+		} 
+		else 
+		if (type & SPACE) {
+			sign = ' ';
+			size--;
+		}
 	}
-	while(value_fix);
-	
-	if (value < 0) 
-		strbuf[len++] = '-'; 
 
-	for (int index = 1 ; index <= len/2; ++index)
-	{
-		char reverse = strbuf[len  - index];  
-		strbuf[len - index] = strbuf[index -1];   
-		strbuf[index - 1] = reverse; 
+	if (type & SPECIAL) {
+		if (base == 16)
+			size -= 2;
+		else
+		if (base == 8)
+			size--;
 	}
 
-	return len;
-}
+	chgsize = 0;
+	unum = (unsigned)num;
+	do {
+		tmp[chgsize] = (digits[unum % base] | locase) ;
+		++chgsize;
+		unum /= base;
+	}while(unum);
 
-#else
+	if (precision < chgsize)
+		precision = chgsize;
 
-/**
-	* @author   古么宁
-	* @brief    i_itoa 
-	*           整型转十进制字符串
-	* @param    strbuf   转字符串所在内存
-	* @param    value  值
-	* @return   转换所得字符串长度
-*/	
-int i_itoa(char * out,int value)		
-{
-	char buf[36];
-	char *strbuf = buf;
-	int value_fix = (value < 0) ? (0-value) : value; 
-	
-	do
-	{
-		*strbuf++ = (char)(value_fix % 10 + '0'); 
-		value_fix = value_fix/10;
-	}
-	while(value_fix);
-	
-	if (value < 0) 
-		*strbuf++ = '-'; 
-
-	for (char * reverse = strbuf-1 ; reverse >= buf ; *out++ = *reverse--);
-	
-	return strbuf - buf;
-}
-
-
-#endif
-
-
-/**
-	* @author   古么宁
-	* @brief    i_ftoa 
-	*           浮点型转字符串，保留4位小数
-	* @param    strbuf   转字符串所在内存
-	* @param    value  值
-	* @return   字符串长度
-*/
-int i_ftoa(char * strbuf,float value)		
-{
-	int len = 0;
-	float value_fix = (value < 0.0f )? (0.0f - value) : value; 
-	int int_part   = (int)value_fix;  
-	int float_part =  (int)(value_fix * 10000) - int_part * 10000;
-
-	for(uint32_t i = 0 ; i < 4 ; ++i)
-	{		
-		strbuf[len++] = (char)(float_part % 10 + '0');
-		float_part = float_part / 10;
+	size -= precision;
+	if (!(type & (ZEROPAD + LEFT))){
+		for ( ; size > 0 ; --size )
+			*str++ = ' ';
 	}
 	
-	strbuf[len++] = '.';  
+	if (sign)
+		*str++ = sign;
+	
+	if (type & SPECIAL) {
+		if (base == 8)
+			*str++ = '0';
+		else 
+		if (base == 16) {
+			*str++ = '0';
+			*str++ = ('X' | locase);
+		}
+	}
 
-	do
-	{
-		strbuf[len++] = (char)(int_part % 10 + '0'); 
-		int_part = int_part/10;
+	if (!(type & LEFT)){
+		for ( ; size > 0 ; --size)
+			*str++ = padding ;
 	}
-	while(int_part);            
-	
-	if (value < 0.0f) 
-		strbuf[len++] = '-'; 
-	
-	for (int index = 1 ; index <= len/2; ++index)
-	{
-		char reverse = strbuf[len  - index];  
-		strbuf[len - index] = strbuf[index -1];   
-		strbuf[index - 1] = reverse; 
-	}
-	
-	return len;
-}
 
+	for ( ; chgsize < precision ; --precision ) 
+		*str++ = '0';
 
-/**
-	* @author   古么宁
-	* @brief    i_itoa 
-	*           整型转十六进制字符串
-	* @param    strbuf   转字符串所在内存
-	* @param    value  值
-	* @return   转换所得字符串长度
-*/	
-int i_xtoa(char * strbuf,uint32_t value)		
-{
-	int len = 0;
-	
-	do{
-		char ascii = (char)((value & 0x0f) + '0');
-		strbuf[len++] = (ascii > '9') ? (ascii + 7) : (ascii);
-		value >>= 4;
-	}
-	while(value);
-	
-	for (int index = 1 ; index <= len/2; ++index)
-	{
-		char reverse = strbuf[len  - index];  
-		strbuf[len - index] = strbuf[index -1];   
-		strbuf[index - 1] = reverse; 
-	}
-	
-	return len;
+	for ( ; chgsize > 0 ; *str++ = tmp[--chgsize]);
+
+	for ( ; size > 0 ; --size)
+		*str++ = ' ' ;
+
+	return str;
 }
 
 
 
-/**
-	* @author   古么宁
-	* @brief    printk 
-	*           格式化输出，类似于 sprintf 和 printf 
-	*           用标准库的 sprintf 和 printf 的方法太慢了，所以自己写了一个，重点要快
-	* @param    fmt     要格式化的信息字符串指针
-	* @param    ...     不定参
-	* @return   void
-*/
+static char * float2string(char *str,float num, int size, int precision,int type)
+{
+	char tmp[66];
+	char sign,padding;
+	int chgsize;
+	unsigned int ipart ;
+
+	if (type & LEFT)
+		type &= ~ZEROPAD;
+
+	padding = (type & ZEROPAD) ? '0' : ' ';
+
+	if (precision < 0 || precision > 6) // 精度，此处精度限制为最多 6 位小数
+		precision = 6;
+
+	if (num < 0.0f) {  // 如果是负数，则先转换为正数，并占用一个字节存放负号
+		sign = '-';
+		num  = -num ;
+		size--;
+	}
+	else 
+		sign = 0;
+
+	chgsize = 0;
+	ipart = (unsigned int)num; // 整数部分
+
+	if (precision) {           // 如果有小数转换，则提取小数部分
+		static const float mulf[7] = {1,10,100,1000,10000,100000,1000000};
+		static const unsigned int muli[7] = {1,10,100,1000,10000,100000,1000000};
+		unsigned int fpart = (unsigned int)(num * mulf[precision]) - ipart * muli[precision];
+		for(int i = 0 ; i < precision ; ++i) {		
+			tmp[chgsize++] = (char)(fpart % 10 + '0');
+			fpart /= 10;
+		}
+		tmp[chgsize++] = '.';
+	}
+
+	do {
+		tmp[chgsize++] = (char)(ipart % 10 + '0');
+		ipart /= 10;
+	}while(ipart);
+
+	size -= chgsize;                 // 剩余需要填充的大小
+
+	if (!(type & LEFT)){             // 右对齐
+		if ('0' == padding && sign) {// 如果是填充 0 且为负数，先放置负号
+			*str++ = sign;
+			sign = 0;
+		}
+		for ( ; size > 0 ; --size)   // 填充
+			*str++ = padding ;
+	}
+
+	if (sign)
+		*str++ = sign;
+
+	for ( ; chgsize > 0 ; *str++ = tmp[--chgsize]);
+
+	for ( ; size > 0 ; --size)   // 左对齐时，填充右边的空格
+		*str++ = ' ' ;
+
+	return str;
+}
+
+
+#include <ctype.h>
+
 void printk(const char* fmt, ...)
+//void newprintk(const char* fmt, ...)
+//int vsprintf(char *buf, const char *fmt, va_list args)
 {
-	char * buf_head = fmt;
-	char * buf_tail = fmt;
+	int len , base;
+	unsigned long num;
+	const char * substr;
+	int flags;          /* flags to number() */
+	int field_width;    /* width of output field */
+	int precision;      /* min. # of digits for integers; max
+                           number of chars for from string */
+	int qualifier;      /* 'h', 'l', or 'L' for integer fields */
 
-	if (NULL == current_puts) 
-		return ;
-	
-	va_list ap; 
-	va_start(ap, fmt);
+	char * fmthead = (char *)fmt;
+	char * fmtout = fmthead;
 
-	while ( *buf_tail )
-	{
-		if ('%' == *buf_tail) //遇到格式化标志,为了效率仅支持 %d ,%f ,%s %x ,%c 
-		{
-			char  buf_malloc[64] = { 0 };//把数字转为字符串的缓冲区
-			char *buf = buf_malloc;  //把数字转为字符串的缓冲区
-			int   len = 0;   //最终转换长度
-			
-			if (buf_tail != buf_head)    //先把 % 前面的部分输出
-				current_puts(buf_head,buf_tail - buf_head);
-	
-			buf_head = buf_tail++;
-			switch (*buf_tail++) // 经过两次 ++, buf_tail 已越过 %#
-			{
-				case 'd': //输出整数的用法最多，所以把 %d 放第一位
-					len = i_itoa(buf,va_arg(ap, int));
-					break;
+	va_list args;
+	va_start(args, fmt);
 
-				case 'x': //输出十六进制整数
-					len = i_xtoa(buf,va_arg(ap, int));
-					break;
-				
-				case 's': //输出字符串
-					buf = va_arg(ap, char*);
-					len = strlen(buf);
-					break;
+	for ( ; *fmtout; ++fmtout) {
+		if (*fmtout == '%') {
+			char tmp[128] ;
+			char * str = tmp ;
+
+			if (fmthead != fmtout)   { //先把 % 前面的部分输出
+				current_puts(fmthead,fmtout - fmthead);
+				fmthead = fmtout;
+			}
+
+			/* process flags */
+			flags = 0;
+			base  = 0;
+			do {
+				++fmtout; /* this also skips first '%' */
+				switch (*fmtout) {
+					case '-': flags |= LEFT;    break;
+					case '+': flags |= PLUS;    break;
+					case ' ': flags |= SPACE;   break;
+					case '#': flags |= SPECIAL; break;
+					case '0': flags |= ZEROPAD; break;
+					default : base = 1;
+				}
+			}while(!base);
+
+			/* get field width */
+			for (field_width = 0 ; isdigit(*fmtout) ; ++fmtout) 
+				field_width = field_width * 10 + *fmtout - '0';
+
+			/* get the precision */
+			if (*fmtout == '.') {
+				precision = 0;
+				for (++fmt ; isdigit(*fmtout) ; ++fmtout) 
+					precision = precision * 10 + *fmtout - '0';
+			}
+			else
+				precision = -1;
+
+			/* get the conversion qualifier *fmt == 'h' ||  || *fmt == 'L'*/
+			qualifier = -1;
+			if (*fmtout == 'l') {
+				qualifier = *fmtout;
+				++fmtout;
+			}
+
+			/* default base */
+			base = 10;
+
+			switch (*fmtout) {
+				case 'c':
+					if (!(flags & LEFT))
+						for ( ; --field_width > 0 ; *str++ = ' ') ;
+					*str++ = (unsigned char)va_arg(args, int);
+					for ( ; --field_width > 0 ; *str++ = ' ') ;
+					current_puts(tmp,str-tmp);
+					fmthead = fmtout + 1;
+					continue;
+
+				case 's':
+					substr = va_arg(args, char *); 
+					if (!substr)
+						substr = "(NULL)";
+					len = strnlen(substr, precision);
+					if ((!(flags & LEFT)) && (len < field_width)){
+						for ( ; len < field_width ; --field_width)
+							*str++ = ' ';
+						current_puts(tmp,str-tmp);
+					} 
+					current_puts(substr,len);
+					if (len < field_width) {
+						for ( ; len < field_width ; --field_width)
+							*str++ = ' ';
+						current_puts(tmp,str-tmp);
+					}
+					fmthead = fmtout + 1;
+					continue;
+
+				case 'p':
+					if (field_width == 0) {
+						field_width = 2 * sizeof(void *);
+						flags |= ZEROPAD;
+					}
+					str = number(tmp,
+						(unsigned long)va_arg(args, void *), 16,
+						field_width, precision, flags);
+					current_puts(tmp,str-tmp);
+					fmthead = fmtout + 1;
+					continue;
 
 				case 'f':
-					len = i_ftoa(buf,(float)va_arg(ap, double));
-					break;
-				
-				case 'c' :
-					buf[len++] = (char)va_arg(ap, int);
+					str = float2string(tmp,va_arg(args, double),field_width, precision, flags);
+					current_puts(tmp,str-tmp);
+					fmthead = fmtout + 1;
+					continue;
+
+				/* case '%':
+					*str++ = '%';
+					continue;*/
+
+				/* integer number formats - set up the flags and "break" */
+				case 'o':
+					base = 8;
 					break;
 
-				case '\0' :
-					goto print_end ;
+				case 'x':
+					flags |= SMALL;
+				case 'X':
+					base = 16;
+					break;
 
-				default:continue; 
+				case 'd':
+				case 'i':
+					flags |= SIGN;
+				case 'u':
+					break;
+
+				default:
+					*str++ = '%';
+					if (*fmt)
+						*str++ = *fmt;
+					else
+						--fmt;
+					continue;
 			}
-			
-			buf_head = buf_tail;
-			current_puts(buf,len);
-		}
-		else  //if ('%' != *buf_tail)
-		{
-			++buf_tail;//普通字符
+
+			if (qualifier == 'l')
+				num = va_arg(args, unsigned long);
+			else 
+				num = va_arg(args, int);
+
+			str = number(tmp, num, base, field_width, precision, flags);
+			current_puts(tmp,str-tmp);
+			fmthead = fmtout + 1;
 		}
 	}
-
-print_end:
-	va_end(ap);
-	
-	if (buf_tail != buf_head) 
-		current_puts(buf_head,buf_tail - buf_head);
-}
-
-
-
-/**
-	* @author   古么宁
-	* @brief    sprintk
-	*           格式化输出，类似于 sprintf 和 printf
-	*           用标准库的 sprintf 和 printf 的方法太慢了，所以自己写了一个，重点要快
-	* @param    fmt     要格式化的信息字符串指针
-	* @param    ...     不定参
-	* @return   void
-*/
-int sprintk(char * buffer ,const char * fmt , ...)
-{
-	char * bufs = buffer;
-	char * copy = (char *)fmt;
-
-	va_list ap; 
-	va_start(ap, fmt);
-
-	while ( *copy )
-	{
-		if ('%' == *copy) //遇到格式化标志,为了效率仅支持 %d ,%f ,%s %x ,%c 
-		{
-			char  buf_malloc[32] ;//把数字转为字符串的缓冲区
-			char *buf = buf_malloc;  //把数字转为字符串的缓冲区
-			int   len = 0;   //最终转换长度
-	
-			switch (copy[1])
-			{
-				case 'd': //输出整数的用法最多，所以把 %d 放第一位
-					len = i_itoa(buf,va_arg(ap, int));
-					break;
-
-				case 'x': //输出十六进制整数
-					len = i_xtoa(buf,va_arg(ap, int));
-					break;
-				
-				case 's': //输出字符串
-					buf = va_arg(ap, char*);
-					len = strlen(buf);
-					break;
-
-				case 'f':
-					len = i_ftoa(buf,(float)va_arg(ap, double));
-					break;
-				
-				case 'c' :
-					buf[len++] = (char)va_arg(ap, int);
-					break;
-
-				case '\0' :
-					*buffer++  = '%';
-					goto sprint_end;
-
-				default://普通字符
-					*buffer++ = *copy++;
-					continue; 
-			}
 			
-			for (copy += 2 ; len-- ; *buffer++ = *buf++) ;
-		}
-		else  //if ('%' != *buf_tail)
-		{
-			*buffer++ = *copy++;//普通字符
-		}
+	if (fmthead != fmtout)   {
+		current_puts(fmthead,fmtout - fmthead);
+		fmthead = fmtout;
 	}
-
-sprint_end:
-
-	va_end(ap);
-
-	*buffer++  = '\0';
-	return (buffer - bufs);// buffer
 }
-
-
-
 
